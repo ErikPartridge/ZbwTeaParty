9f54e2e96d9f41b0628a9e945821a2c27533b628
Erik Malmstrom-Partridge <erikdevelopments@gmail.com>
2015-06-07 17:37:44 -0400

    Sort bookings, add mail

--- resources/views/bookings.blade.php
+++ resources/views/bookings.blade.php
@@ -12,6 +12,7 @@
   		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab|Open+Sans' rel='stylesheet' type='text/css'>
   		<link rel="stylesheet" href="/css/main.css">
       <link rel="stylesheet" href="/css/fids.css">
+      <script src="https://raw.githubusercontent.com/tristen/tablesort/gh-pages/tablesort.min.js"></script>
 	</head>
 	<body>
 		<div class="navbar-fixed">
@@ -57,7 +58,7 @@
         <p><b>A:</b>&nbsp;Yes, but not yet. While you don't need a booking to fly, if you'd still like to have the booking swag, pilots will be allowed to create a booking starting in mid-June.</p>
         <h3>Bookings</h3>
         <b>ALL TIMES ARE ZULU (EDT+4, PDT+7)</b>
-        <table class="fid-box">
+        <table id="bookings-table" class="fid-box">
           <thead><th>Dept. Time</th><th>Arr. Time</th><th>Flight No.</th><th>Departs</th><th>Destination</th><th>Status</th></thead>
           @foreach($flights as $flight)
           <tr><td>{{substr($flight->departure, 0, 5)}}z</td><td>{{substr($flight->arrival, 0, 5)}}z</td><td>{{$flight->callsign}}</td><td>{{$flight->departs}}</td><td>{{$flight->arrives}}</td>@if($flight->booked)
@@ -93,6 +94,21 @@
 	            </div>
 	          </div>
 	        </footer>
+
+          <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63783867-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script>
+  new Tablesort(document.getElementById('bookings-table'));
+</script>
+
 	<script>
 		$(document).ready(function(){
 
