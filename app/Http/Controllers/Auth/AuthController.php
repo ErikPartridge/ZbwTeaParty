c72bdb2031bc5c3fbbef09c325c08d884ad1b1ff
Erik Malmstrom-Partridge <erikdevelopments@gmail.com>
2015-06-10 20:21:32 -0400

    Forget what I just said

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -35,4 +35,12 @@ class AuthController extends Controller {
 		$this->middleware('guest', ['except' => 'getLogout']);
 	}
 
+	public function getRegister(){		
+		return redirect('/');		
+	}		
+		
+	public function postRegister(){		
+		return redirect('/');		
+	}
+
 }
