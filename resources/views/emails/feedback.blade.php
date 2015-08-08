1ded823cc92b6266e0c7a86b7db1c3093f708f77
Erik Malmstrom-Partridge <erikdevelopments@gmail.com>
2015-08-07 22:49:48 -0400

    more fixes

---
+++ resources/views/emails/feedback.blade.php
@@ -0,0 +1,15 @@
+Dear DB,
+
+Name: {{$name}}
+CID: {{$cid}}
+Email: {{$email}}
+
+For positon:
+
+Position: {{$position}}
+Rating: {{$rating}}
+Comments: {{$comments}}
+
+Response: (1 if yes, 0 if no) {{$response}}
+
+Thanks
